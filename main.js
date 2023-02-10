f8e4c1e2eca4710671b728a5dc7183fc1ff6123f
kacasih <122456578+kacasih@users.noreply.github.com>
2023-02-10 01:20:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function startgame() {
   const catCharacterselect = document.querySelector(".cat-character");
   let isAnimating = false;
-
   document.addEventListener("keydown", (event) => {
     if (event.code === "Space" && !isAnimating) {
       isAnimating = true;
@@ -12,20 +11,24 @@ function startgame() {
       }, 750);
     }
   });
-}
+
 
 
 var collisioncheck = setInterval(function() {
-  var catdata = catCharacterselect.getBoundingClientRect();
-  var obstacledata = obstacle.getBoundingClientRect();
-
-  if (catdata.right > obstacledata.left && 
-      catdata.left < obstacledata.right && 
-      catdata.bottom > obstacledata.top && 
-      catdata.top < obstacledata.bottom ) {
+  let obstacle = document.querySelector(".obstacle");
+  let catdata = catCharacterselect.getBoundingClientRect();
+  let obstacledata = obstacle.getBoundingClientRect();
+  const outputmessage = document.querySelector(".endgamemessage");
+  if (catdata.right >= obstacledata.left && 
+      catdata.left <= obstacledata.right && 
+      catdata.bottom >= obstacledata.top && 
+      catdata.top <= obstacledata.bottom ) {
     obstacle.style.animation = "none";
     obstacle.style.display = "none";
-    alert("well played");
+    catCharacterselect.style.animation = "none";
+    catCharacterselect.style.display = "none";
+    outputmessage.innerHTML = "well played";
   } 
 
 }, 50);
+}
