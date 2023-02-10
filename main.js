423a45ad50fd0744aff55df77114442a915c2ec2
kacasih <122456578+kacasih@users.noreply.github.com>
2023-02-10 02:48:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function startgame() {
   const catCharacterselect = document.querySelector(".cat-character");
+  const obstacle = document.querySelector(".obstacle");
+  const outputmessage = document.querySelector(".endgamemessage");
   let isAnimating = false;
   document.addEventListener("keydown", (event) => {
     if (event.code === "Space" && !isAnimating) {
@@ -11,24 +13,36 @@ function startgame() {
       }, 750);
     }
   });
-
-
-
-var collisioncheck = setInterval(function() {
-  let obstacle = document.querySelector(".obstacle");
-  let catdata = catCharacterselect.getBoundingClientRect();
-  let obstacledata = obstacle.getBoundingClientRect();
-  const outputmessage = document.querySelector(".endgamemessage");
-  if (catdata.right >= obstacledata.left && 
-      catdata.left <= obstacledata.right && 
-      catdata.bottom >= obstacledata.top && 
-      catdata.top <= obstacledata.bottom ) {
-    obstacle.style.animation = "none";
-    obstacle.style.display = "none";
-    catCharacterselect.style.animation = "none";
-    catCharacterselect.style.display = "none";
-    outputmessage.innerHTML = "well played";
-  } 
-
-}, 50);
+  var collisioncheck = setInterval(function() {
+    let catdata = catCharacterselect.getBoundingClientRect();
+    let obstacledata = obstacle.getBoundingClientRect();
+    const outputmessage = document.querySelector(".endgamemessage");
+    if (catdata.right >= obstacledata.left && 
+        catdata.left <= obstacledata.right && 
+        catdata.bottom >= obstacledata.top && 
+        catdata.top <= obstacledata.bottom ) {
+      obstacle.style.animation = "none";
+      obstacle.style.display = "none";
+      catCharacterselect.style.animation = "none";
+      catCharacterselect.style.display = "none";
+      outputmessage.innerHTML = "well played";
+    }
+  }, 50);
+  
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "KeyR") {
+      obstacle.style.animation = "";
+      obstacle.style.display = "";
+      catCharacterselect.style.animation = "";
+      catCharacterselect.style.display = "";
+      outputmessage.innerHTML = "";
+      startgame();
+    }
+  });
 }
+
+  /*Wholepage.addEventListener("keypress", (event) => {
+    console.log(`Key Code: ${event.keyCode}`);
+  });
+  alert("sheesh");
+}*/
